dea947d56d125be10791e0ce94236286378ec655
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-11-12 23:14:55 +0100

    [ui] Add function for adding channel tabs

--- public/js/view.js
+++ public/js/view.js
@@ -1,5 +1,20 @@
 view = {};
 
+view.encodeChannelName = function (channel) {
+  return 'channel-' + (channel[0] == '#') ? channel.slice(1) : channel;
+};
+
+view.addChannel = function (channel) {
+  var logTabs = $('#log-tabs');
+  var logContent = $('#log-content');
+
+  var encoded = view.encodeChannelName(channel);
+  logTabs.append('<li id="log-tab-' + encoded + '"><a href="#log-' + encoded +
+                 '">' + channel + '</a></li>');
+
+  logContent.append('<div class="tab-pane" id="log-' + encoded + '"></div>');
+};
+
 view.log = function (source, msg, type) {
   // TODO: redesign it to use plates
   $('#log').append(source + ': ' + msg + '<br />');
