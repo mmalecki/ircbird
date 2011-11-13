71c85a9ca31713c99ff79d02c67bb6d6460db24b
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-11-13 01:04:59 +0100

    [view] Use `data-item` for logging

--- public/js/view.js
+++ public/js/view.js
@@ -19,7 +19,7 @@ view.addTab = function (item) {
 view.log = function (from, to,  msg) {
   // TODO: redesign it to use plate
   var tab = (to[0] == '#') ? to : from;
-  $('#log-' + view.encodeTabName(tab)).append(from + ': ' + msg + '<br />');
+  $('div[data-item="' + tab + '"]').append(from + ': ' + msg + '<br />');
 };
 
 view.login = function () {
